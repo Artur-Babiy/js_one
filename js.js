11f5ed96ca9df41852a22a1bad20f9c099752baa
ArturBabiy <artur.babiy.228@gmail.com>
2020-11-03 15:59:09 +0200

    Initial commit

---
+++ js.js
@@ -0,0 +1,404 @@
+// task #1
+//
+//присвоить каждому из следующих значений свою переменную:
+// 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false
+// Вывести каждую при помощи console.log , alert, document.write
+
+//let str1 = 'hello';
+// let str2 = 'owu';
+// let str3 = 'com';
+// let str4 = 'ua';
+//
+// let num1 = 1;
+// let num2 = 10;
+// let num3 = -999;
+// let num4 = 123;
+// let num5 = 3.14;
+// let num6 = 2.7;
+// let num7 = 16;
+//
+// let bool1 = true;
+// let bool2 = false;
+//
+//console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+//
+// console.log(bool1);
+// console.log(bool2);
+//
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// alert(str4);
+//
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+//
+// alert(bool1);
+// alert(bool2);
+//
+// document.write(str1);
+// document.write(str2);
+// document.write(str3);
+// document.write(str4);
+//
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(num4);
+// document.write(num5);
+// document.write(num6);
+// document.write(num7);
+//
+// document.write(bool1);
+// document.write(bool2);
+
+
+// task #2
+//переопределить каждую переменную из задания 1 дав им произвольные значения Вывести каждую при помощи console.log , alert, document.write
+
+// let str1 = 'hello';
+// str1 = 'world';
+// let str2 = 'owu';
+// str2 = 'university';
+// let str3 = 'com';
+// str3 = 'ua';
+// let str4 = 'ua';
+// str4 = 'com';
+//
+// let num1 = 1;
+// num1 = 2;
+// let num2 = 10;
+// num2 = 20;
+// let num3 = -999;
+// num3 = -1000;
+// let num4 = 123;
+// num4 = 345;
+// let num5 = 3.14;
+// num5 = 3.1415;
+// let num6 = 2.7;
+// num6 = 3;
+// let num7 = 16;
+// num7 = 32;
+//
+// let bool1 = true;
+// bool1 = false;
+// let bool2 = false;
+// bool2 = true;
+//
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// console.log(str4);
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+// console.log(num6);
+// console.log(num7);
+//
+// console.log(bool1);
+// console.log(bool2);
+
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// alert(str4);
+//
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(num4);
+// alert(num5);
+// alert(num6);
+// alert(num7);
+//
+// alert(bool1);
+// alert(bool2);
+
+// document.write(str1);
+// document.write(str2);
+// document.write(str3);
+// document.write(str4);
+//
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(num4);
+// document.write(num5);
+// document.write(num6);
+// document.write(num7);
+//
+// document.write(bool1);
+// document.write(bool2);
+
+// task #3
+//Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+
+// const num1 = 1;
+// const num2 = 5;
+// const num3 = 10;
+//
+// const str1 = 'hello';
+// const str2 = 'world';
+// const str3 = 'com';
+//
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+//
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+//
+// document.write(num1);
+// document.write(num2);
+// document.write(num3);
+// document.write(str1);
+// document.write(str2);
+// document.write(str3);
+
+// task #4
+//при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО.
+// Для фамилии имени и отчества создать разные переменные.
+// Вывести каждую при помощи console.log , alert, document.write
+//
+// let name = prompt('Enter your name');
+// let lastName = prompt('Enter your lastname');
+// let surName = prompt('Enter your surname');
+//
+// console.log(name);
+// console.log(lastName);
+// console.log(surName);
+//
+// alert(name);
+// alert(lastName);
+// alert(surName);
+//
+// document.write(name);
+// document.write(lastName);
+// document.write(surName);
+
+// task #5
+//Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+// let name = prompt('Enter your name');
+// let lastName = prompt('Enter your lastname');
+// let surName = prompt('Enter your surname');
+//
+// let person = name + lastName + surName;
+//
+// console.log(person);
+
+// task #6
+//Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+
+// let name = prompt('Enter your name');
+// let lastName = prompt('Enter your lastname');
+// let surName = prompt('Enter your surname');
+//
+// let i = name + lastName + surName;
+// let brother = name + lastName + surName;
+// let mother = name + lastName + surName;
+// let father = name + lastName + surName;
+// let sister = name + lastName + surName;
+//
+// console.log(i);
+// console.log(brother);
+// console.log(mother);
+// console.log(father);
+// console.log(sister);
+
+// task #7
+//при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+// let num1 = prompt('Enter first number');
+// let num2 = prompt('Enter second number');
+// let num3 = prompt('Enter third number');
+//
+// let num4 = num1 + num2 + num3;
+// console.log(num4);
+
+// task #8
+//при помощи prompt() получить 4 числа.
+// Привести их к числовому типу при помощи parseInt.
+// Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let num1 = prompt('Enter first number');
+// let num2 = prompt('Enter second number');
+// let num3 = prompt('Enter third number');
+// let num4 = prompt('Enter four number');
+//
+// let result = num1 + num2 + num3 + num4;
+//
+// console.log(parseInt(result));
+
+// task #9
+//при помощи prompt()  получить 3 числа с плавающей точекой.
+// при помощи parseFloat привести их к соответсвующему типу.
+// Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let num1 = prompt('Enter first number');
+// let num2 = prompt('Enter second number');
+// let num3 = prompt('Enter third number');
+//
+// let result = num1 + num2 + num3;
+// console.log(parseFloat(result));
+
+// task #10
+//при помощи prompt()  получить 3 числа с плавающей точекой.
+// Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+// let num1 = prompt('Enter first number');
+// let num2 = prompt('Enter second number');
+// let num3 = prompt('Enter third number');
+//
+// num1 = Math.round(num1);
+// num2 = Math.round(num2);
+// num3 = Math.round(num3);
+//
+// let result = num1 + num2 + num3;
+// console.log(result);
+
+// task #11
+//при помощи prompt() получить 2 целых числа.
+// Привести их к целочисленному типу.
+// Первое число - это число которое будут возводить в степень.
+// Второе число - это число которое является степенью.
+// При помощи Math.pow возвести первое число в степень второго числа.
+
+// let num1 = prompt('Enter first number');
+// let num2 = prompt('Enter second number');
+//
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+//
+// let result = Math.pow(num1,num2);
+// console.log(result);
+
+
+// task #12
+//При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// task #13
+//Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+//  В одинаковых выражениях в задании не использовать два одинаковых оператора.
+// Каждое выражение вложить в свою переменную
+//     5 ? 6 -> true
+//     5 ? 6 -> false
+//     5 ? 6 -> false
+//     5 ? 6 -> false
+//     10 ? 10 -> true
+//     10 ? 10 -> true
+//     10 ? 10 -> false
+//     10 ? 10 -> false
+//     10 ? 10 -> false
+//     123 ? '123' -> false
+//     123 ? '123' -> true
+
+
+// let num1 = 5;
+// let num2 = 6;
+// let num3 = 10;
+// let num4 = num3;
+// let num5 = 123;
+// let num6 = '123';
+
+
+// if (num1 < num2) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num1 > num2){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num1 === num2) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num1 >= num2){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// if (num3 === num4) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num3 >= num4){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num3 > num4){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num3 < num4){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+//
+// if (num3 !== num4){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// if (num5 === num6){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+// if (num5 !== num6){
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
